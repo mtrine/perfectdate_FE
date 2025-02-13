23d382bc305cf45f6febe0d8c45338e1ac0425d3
Mtri nèeeeeee <90810549+mtrine@users.noreply.github.com>
2025-02-13 08:18:30 +0700

    Header homepage

--- src/app/(user)/home/layout.tsx
+++ src/app/(user)/home/layout.tsx
@@ -5,9 +5,7 @@ export default function HomeLayout({ children }: { children: React.ReactNode }) 
     return (
         <div className="bg-skin min-h-screen">
             <Header />
-            <div className="pt-[80px]"> {/* Thêm padding-top để tránh bị đè */}
                 {children}
-            </div>
             <Footer />
         </div>
     );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 import LogoImage from "@/assets/logo-not-bg.png"
 export default function Header() {
     return (
-        <header className="fixed  top-[50px] left-0 right-0 z-50 flex justify-center">
-            <nav className=" flex justify-between bg-green rounded-full pr-[50px] pl-[50px] w-[70%]    ">
-            <div className="container-img-logo flex flex-row   ">
-                <div className="flex items-center">
+        <header className="h-24">
+            <nav className="fixed top-[10px] left-0 right-0 z-50 flex justify-center ">
+               <div className="flex justify-between bg-green rounded-full pr-[50px] pl-[50px] w-[70%]  items-center shadow-md ">
+               <div className="flex items-center">
                     <Image
                         src={LogoImage}
                         alt="Perfect Date Logo"
@@ -16,22 +16,14 @@ export default function Header() {
                         priority
                     />
                 </div>
-            </div>
                 <ul className="flex items-center space-x-4">
-                    <li>
-                        <a href="#" className="text-blue hover:text-white text-[15px] ">Trang chủ</a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-blue hover:text-white text-[15px]">Khám phá</a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-blue hover:text-white text-[15px]">Liên hệ</a>
-                    </li>
+                    <li><a href="#" className="text-blue hover:text-white">Trang chủ</a></li>
+                    <li><a href="#" className="text-blue hover:text-white">Khám phá</a></li>
+                    <li><a href="#" className="text-blue hover:text-white">Liên hệ</a></li>
                 </ul>
-            <div className="flex items-center">
-               <button className="border px-4 py-2 rounded-full">Đăng nhập</button>
-            </div>
-        </nav>
+                <button className="border px-4 py-2 rounded-full">Đăng nhập</button>
+               </div>
+            </nav>
         </header>
     );
 }
