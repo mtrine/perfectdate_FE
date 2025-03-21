60176e91d6b170956b189c2760a07ca394167a3f
Mtri nèeeeeee <90810549+mtrine@users.noreply.github.com>
2025-03-21 12:36:46 +0700

    Logic handle not activities

--- src/app/(user)/dashboard/plan-detail/[id]/page.tsx
+++ src/app/(user)/dashboard/plan-detail/[id]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import Button from '@/components/Button';
+import ActivitiesInPlan from '@/components/user/ActivitesInPlan';
 import PlanActivityItem from '@/components/user/PlanActivityItem';
 import { useParams } from 'next/navigation';
 
 export default function PlanDetail() {
   const { id } = useParams();
+  const haveActivity = false;
   return (
     <div className="flex flex-col  pt-20 max-w-[70%] mx-auto min-h-screen  space-y-[2%]">
       <h2 className="text-darkRed text-center">Chi tiết kế hoạch</h2>
@@ -12,54 +14,10 @@ export default function PlanDetail() {
         <h1>Lịch trình Vũng Tàu nè!</h1>
         <h1>24/02/2025</h1>
       </div>
-      <div className="space-y-[5%]">
-        <PlanActivityItem
-          time="04:00 SA"
-          content="TP.HCM - Vũng Tàu"
-          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
-          note=""
-        />
-
-        <PlanActivityItem
-          time="04:00 SA"
-          content="TP.HCM - Vũng Tàu"
-          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
-          note=""
-        />
-        <PlanActivityItem
-          time="04:00 SA"
-          content="TP.HCM - Vũng Tàu"
-          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
-          note=""
-        />
-        <PlanActivityItem
-          time="04:00 SA"
-          content="TP.HCM - Vũng Tàu"
-          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
-          note=""
-        />
-        <PlanActivityItem
-          time="04:00 SA"
-          content="TP.HCM - Vũng Tàu"
-          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
-          note=""
-        />
-      </div>
-      <div>
-        <p>
-          <strong className="underline text-darkRed">Mô tả :</strong> Mô tả nè
-        </p>
-      </div>
-      <div className='flex justify-center space-x-4 items-center'>
-        <Button
-            text='Chỉnh sửa'
-            typeButton='secondary'
-        />
-        <Button
-            text='Chia sẻ công khai'
-            typeButton='secondary'
-            color='cream'
-        />
+      {haveActivity ? <ActivitiesInPlan /> : <p>Không có hoạt động nào</p>}
+      <div className="flex justify-center space-x-4 items-center">
+        <Button text="Chỉnh sửa" typeButton="secondary" />
+        <Button text="Chia sẻ công khai" typeButton="secondary" color="cream" />
       </div>
     </div>
   );
---
+++ src/components/user/ActivitesInPlan.tsx
@@ -0,0 +1,46 @@
+import PlanActivityItem from "./PlanActivityItem";
+
+export default function ActivitiesInPlan() {
+  return (
+    <>
+      <div className="space-y-[5%]">
+        <PlanActivityItem
+          time="04:00 SA"
+          content="TP.HCM - Vũng Tàu"
+          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
+          note=""
+        />
+
+        <PlanActivityItem
+          time="04:00 SA"
+          content="TP.HCM - Vũng Tàu"
+          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
+          note=""
+        />
+        <PlanActivityItem
+          time="04:00 SA"
+          content="TP.HCM - Vũng Tàu"
+          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
+          note=""
+        />
+        <PlanActivityItem
+          time="04:00 SA"
+          content="TP.HCM - Vũng Tàu"
+          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
+          note=""
+        />
+        <PlanActivityItem
+          time="04:00 SA"
+          content="TP.HCM - Vũng Tàu"
+          title="Trên đường ghé ăn sáng và nghỉ ngơi ngắm cảnh mất tầm 3 tiếng"
+          note=""
+        />
+      </div>
+      <div>
+        <p>
+          <strong className="underline text-darkRed">Mô tả :</strong> Mô tả nè
+        </p>
+      </div>
+    </>
+  );
+}
